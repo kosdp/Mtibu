42fbb912e16e4497fc81903ab25dbdc94c384609
kosdp <kosdp@github.com>
2017-01-17 19:07:10 +0200

    Organize imports

--- src/com/example/mtsbu/Mtsbu.java
+++ src/com/example/mtsbu/Mtsbu.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class Mtsbu extends Activity {
     /**
