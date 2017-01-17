91a814a1011212f325cac914dd0e9c2feb89dcd9
kosdp <kosdp@github.com>
2017-01-17 17:25:16 +0200

    Initial commit

---
+++ src/com/example/mtibu/Mtibu.java
@@ -0,0 +1,15 @@
+package com.example.mtibu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Mtibu extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
